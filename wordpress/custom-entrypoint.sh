6c9a650013080b5c8be98d8381cf01d05082dfc9
Sulejman <sulejman.husaenov@most-finance.com>
2025-06-25 13:04:08 +0300

    init

--- wordpress/custom-entrypoint.sh
+++ wordpress/custom-entrypoint.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -e
 
-# Инициализация тома (если пустой)
-if [ -z "$(ls -A /bitnami/wordpress)" ]; then
-    echo "Initializing WordPress volume"
-    # Копируем, преобразуя симлинки в реальные файлы
-    cp -rL /opt/bitnami/wordpress/. /bitnami/wordpress/
-fi
+cp -r --no-preserve=mode,ownership,timestamps /bitnami/wordpress/. /opt/bitnami/wordpress/
 
-# Основное копирование при каждом запуске
-echo "Copying WordPress data from volume to working directory"
-
-# Копируем с преобразованием симлинков
-cp -rL --no-preserve=mode,ownership,timestamps /bitnami/wordpress/. /opt/bitnami/wordpress/
-
-# Запуск оригинального entrypoint
 if [[ $# -eq 0 ]]; then
-    set -- "/opt/bitnami/scripts/apache/run.sh"
+  set -- "/opt/bitnami/scripts/apache/run.sh"
 fi
 
 exec /opt/bitnami/scripts/wordpress/entrypoint.sh "$@"
